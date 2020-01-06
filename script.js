da7709fca38a52e5f5bf7a9b03ed1eff25a375ee
Yoann Picquenot <yoann.picquenot@gmail.com>
2020-01-06 17:38:23 +0100

    WIP

---
+++ script.js
@@ -0,0 +1,12 @@
+(() => {
+    document.querySelector('.toggle').onclick = (event) => {
+        const card = document.querySelector('.card');
+
+        if (card.classList.contains('reverse')) {
+            card.classList.remove('reverse');
+        } else {
+            card.classList.add('reverse');
+        }
+        
+    };
+})();
